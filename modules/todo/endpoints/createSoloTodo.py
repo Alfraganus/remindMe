689562368d7275273f94102489b522e2c37ec2d4
Gulomjon Sulaymonov <uzpsychologist@gmail.com>
2023-11-08 00:39:21 +0500

    starting todo solo

--- modules/todo/endpoints/createSoloTodo.py
+++ modules/todo/endpoints/createSoloTodo.py
@@ -4,6 +4,7 @@ from config.db import MongoDBConnection
 async def createSoloTodo():
     my_collection =  MongoDBConnection.get_collection("todo-solo")
 
-    await my_collection.insert_one({"test": "hello world222"})
-    result = await my_collection.find_one({"test": "hello world222"})  # Change the query here
-    return {"test": str(result)}
+    # await my_collection.insert_one({"test": "hello world222"})
+    result = await my_collection.find_one({"test": "hello world222"}, {'_id': 0})  # Change the query here
+    # result = await my_collection.find_one({"test": "hello world222"})  # Change the query here
+    return {"test": result}
