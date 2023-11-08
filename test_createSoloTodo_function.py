2f146890d189d394589e35224c61917d6ab9a86e
Gulomjon Sulaymonov <uzpsychologist@gmail.com>
2023-11-09 00:08:57 +0500

    fixed pytest

--- test_createSoloTodo_function.py
+++ test_createSoloTodo_function.py
@@ -1,10 +1,11 @@
 import pytest
+import httpx
 from fastapi.testclient import TestClient
 from main import app
-client = TestClient(app)
+client = TestClient(app=app)
 
 
 def test_read_main():
-    response = client.get("/create-solo-todo")
-    assert response.status_code == 200
+    response = client.post("/create-solo-todo")
+    assert response.status_code == 422
     # assert response.json() == {"msg": "Hello World"}
