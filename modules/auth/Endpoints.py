73eb35ade71fb2648bb109974257883c87291337
Gulomjon Sulaymonov <uzpsychologist@gmail.com>
2023-11-12 17:34:47 +0500

    starting todo solo

--- modules/auth/Endpoints.py
+++ modules/auth/Endpoints.py
@@ -1,23 +1,23 @@
 from fastapi import HTTPException, Depends, APIRouter
 from fastapi.security import OAuth2PasswordRequestForm
-from pydantic.typing import Annotated
+# from pydantic.typing import Annotated
 from modules.auth.schemes import UserRegistration
 from modules.auth.services.AuthService import users_collection, UserLogin, password_context, create_access_token, \
     get_current_user
 
 appAuth = APIRouter();
 
-@appAuth.post("/token",tags=["authentication"])
-async def token(
-    form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
-):
-    user = await users_collection.find_one({"username": form_data.username})
-    if user is None or not password_context.verify(form_data.password, user.get("password_hash")):
-        raise HTTPException(status_code=400, detail="Incorrect password")
-    return {
-        "access_token": create_access_token({"username": user["username"]}),
-        "token_type": "bearer"
-    }
+# @appAuth.post("/token",tags=["authentication"])
+# async def token(
+#     form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+# ):
+#     user = await users_collection.find_one({"username": form_data.username})
+#     if user is None or not password_context.verify(form_data.password, user.get("password_hash")):
+#         raise HTTPException(status_code=400, detail="Incorrect password")
+#     return {
+#         "access_token": create_access_token({"username": user["username"]}),
+#         "token_type": "bearer"
+#     }
 
 @appAuth.post("/register", tags=["authentication"])
 async def register(user_registration: UserRegistration):
