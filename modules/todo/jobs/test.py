ea481b5b566b203006252ab38a4eb69fcc2f4298
Gulomjon Sulaymonov <uzpsychologist@gmail.com>
2023-11-14 00:04:49 +0500

    starting todo solo

--- modules/todo/jobs/test.py
+++ modules/todo/jobs/test.py
@@ -1,7 +1,26 @@
+import datetime
+import requests
 from apscheduler.schedulers.background import BackgroundScheduler
 
-def job():
-    print("I'm working...")
+
+
+def test():
+    telegram_api_url = "https://api.telegram.org/bot6734724387:AAF_hb0RnTlkuQKxjUgQadQc5wXPBf-VEkk/sendMessage"
+    chat_id = "1244682"
+    text = "hello world"
+
+    payload = {
+        'chat_id': chat_id,
+        'text': text
+    }
+    response = requests.get(telegram_api_url, params=payload)
+
+    # Check the response status if needed
+    if response.status_code == 200:
+        print(f"Message sent successfully at {datetime.datetime.now()}")
+    else:
+        print(f"Failed to send message. Status code: {response.status_code}")
+
 
 schedular = BackgroundScheduler()
-schedular.add_job(job,'interval',seconds=5)
+schedular.add_job(test,'interval',seconds=5)
