5e6c65fed741229b813c7e2143379a70a53bfa5b
Gulomjon Sulaymonov <uzpsychologist@gmail.com>
2023-11-18 23:55:39 +0500

    starting todo solo

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from modules.auth.Endpoints import appAuth
 from modules.todo.Routes import todoAuth
@@ -7,6 +8,19 @@ from modules.todo.jobs.test import schedular
 app = FastAPI()
 app.include_router(appAuth)
 app.include_router(todoAuth)
+
+origins = [
+    "*",
+    "https://new.spector77.uz"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # schedular.start()
 # test
 
